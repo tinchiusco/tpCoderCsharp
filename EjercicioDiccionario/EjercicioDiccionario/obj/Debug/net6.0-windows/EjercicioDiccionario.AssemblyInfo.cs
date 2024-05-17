fc958a80332877e63ef975342f363841a5ed2012
Martin Julio Rivas Anaya <tinchillo@gmail.com>
2024-05-17 18:35:23 -0300

    tp1.1

--- EjercicioDiccionario/EjercicioDiccionario/obj/Debug/net6.0-windows/EjercicioDiccionario.AssemblyInfo.cs
+++ EjercicioDiccionario/EjercicioDiccionario/obj/Debug/net6.0-windows/EjercicioDiccionario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioDiccionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec4918cfa70a195ce0a31096632bb43c9ffd7ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioDiccionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioDiccionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
